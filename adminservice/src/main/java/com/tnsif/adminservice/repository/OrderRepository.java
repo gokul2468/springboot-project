b57194f1df27469e6c226f3a20f35e910d76069b
Gokul A <165999440+gokul2468@users.noreply.github.com>
2024-05-14 20:03:40 +0530

    Delete adminservice directory

--- adminservice/src/main/java/com/tnsif/adminservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.adminservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.adminservice.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- adminservice/src/main/java/com/tnsif/adminservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.adminservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.adminservice.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
